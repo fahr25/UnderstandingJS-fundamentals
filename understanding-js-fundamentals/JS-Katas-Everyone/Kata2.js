5f2d2e89b2fd6e30320e726af47a8bc10da5ceb6
fahr25 <fhernandez@icstars.org>
2025-10-24 10:33:19 -0500

    Added loop logic and changed it from FizzBuzz to JitterBug.

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -1,9 +1,17 @@
 // Kata 2: FizzBuzz from 1 to 20
 // ** OBJECTIVE** Create your own loop using the following logic**
-// Commit 1: "scaffold for loop"
 // Commit 2: "added fizzbuzz logic"
 // Commit 3: "refactored with clear variable and comments"
 
 for(let i = 1; i <= 20; i++) {
-  console.log(i);
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log("JitterBug!");
+  } else if(i % 3 === 0) {
+    console.log("Jitter");
+  } else if(i% 5 === 0) {
+    console.log("Bug")
+  } else {
+    console.log("Something went wrong...");
+  }
 }
+
