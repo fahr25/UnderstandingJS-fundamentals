cf4fae6fb2e5a57c5a1c549526b0d4f14147eaa7
fahr25 <fhernandez@icstars.org>
2025-10-24 10:41:20 -0500

    Refactored loop and logic for clarity and increased count.

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -1,17 +1,17 @@
-// Kata 2: FizzBuzz from 1 to 20
-// ** OBJECTIVE** Create your own loop using the following logic**
-// Commit 2: "added fizzbuzz logic"
-// Commit 3: "refactored with clear variable and comments"
+/* 
+ * Kata 2: FizzBuzz from 1 to 20
+ * OBJECTIVE** Create your own loop using the following logic**
+*/
 
-for(let i = 1; i <= 20; i++) {
-  if(i % 3 === 0 && i % 5 === 0) {
+for(let someNumber = 1; someNumber <= 30; someNumber++) {
+  if(someNumber % 3 === 0 && someNumber % 5 === 0) {
     console.log("JitterBug!");
-  } else if(i % 3 === 0) {
+  } else if(someNumber % 3 === 0) {
     console.log("Jitter");
-  } else if(i% 5 === 0) {
+  } else if(someNumber % 5 === 0) {
     console.log("Bug")
   } else {
-    console.log("Something went wrong...");
+    console.log(someNumber);
   }
 }
 
