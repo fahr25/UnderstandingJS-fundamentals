33b6104e35c843a99a5db80507658961065b4301
fahr25 <fhernandez@icstars.org>
2025-10-26 22:47:16 -0500

    Implemented a function with safeguards againts negative and large number. Added explanations of what each part does.

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata3.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata3.js
@@ -8,19 +8,56 @@
  * 
 */
 
+// Function with guard clause.
+let multiplicationTableGen = (size) => {
+  console.log(`Your value: ${size}`);
 
-for(let row = 1; row <= 5; row++) {
-  let table = "";
+  if(size <= 0) { // First clause prevents input being less than or equal to 0.
+    return console.log("Please enter a number greater than 0.");
+  } 
+  else if(size > 20) { // Second clause prevents input being greater than 20.
+    return console.log("Do you really need me to multiply any higher?...");
+  } 
+  else {
+    for(let row = 1; row <= size; row++) {
+      // Table variable creates a string version of the table.
+      let table = "";
+      for(let column = 1; column <= size; column++) {
+        // After values are multiplied, a tab space is added.
+        table += (row * column) + "\t";
+      }
 
-  for(let column = 1; column <= 5; column++) {
-    table += (row * column) + "\t";
+      // This print the table.
+      console.log(table);
+    }
   }
 
-  console.log(table);
-}
+};
 
-// Stretch Goal: Guard clause example
-function printNumber(num) {
-  if (num <= 0) return console.log("Please enter a positive number.");
-  console.log("Number:", num);
-}
+
+// ** TESTS ** 
+// This tests for responds when a genative value is used.
+console.log("Test 1");
+multiplicationTableGen(-9);
+console.log()
+
+// This tests for responds when 0 is used as the value.
+console.log("Test 2");
+multiplicationTableGen(0);
+console.log()
+
+// This tests the function with a small value.
+console.log("Test 3");
+multiplicationTableGen(5);
+console.log()
+
+// This test the function above with our desired size but larger.
+console.log("Test 4");
+multiplicationTableGen(18);
+console.log()
+
+// This tests the function above with a value above 20.
+console.log("Test 5");
+multiplicationTableGen(23);
+console.log()
+
